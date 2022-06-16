4e57f81a0fa98b3f666d67b928d43ce7aa40876f
pvig <patricevigouroux@yahoo.fr>
2022-06-16 23:18:29 +0200

    autre mise en page

--- assets/views/Home.vue
+++ assets/views/Home.vue
@@ -2,13 +2,13 @@
   <v-container fluid>
     <v-row style="height: 90vh;">
       <v-col cols="sm" align="center" class="d-flex justify-center align-center">
-        <v-btn outlined x-large color="lime" elevation="2" to="/clients">Clients</v-btn>
+        <v-btn x-large color="teal lighten-4" elevation="2" to="/clients">Clients</v-btn>
       </v-col>
       <v-col cols="sm" align="center" class="d-flex justify-center align-center">
-        <v-btn outlined x-large color="light-green" elevation="2" to="/produits">Produits</v-btn>
+        <v-btn x-large color="teal lighten-3" elevation="2" to="/produits">Produits</v-btn>
       </v-col>
       <v-col cols="sm" align="center" class="d-flex justify-center align-center">
-        <v-btn outlined x-large color="green" elevation="2" to="/ventes">Ventes</v-btn>
+        <v-btn x-large color="teal lighten-4" elevation="2" to="/ventes">Ventes</v-btn>
       </v-col>
     </v-row>
   </v-container>
